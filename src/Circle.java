015ec14ccd799aaf3c8a766f471ad23d1b504da2
Адина <adina.duishobaeva.ch@gmail.com>
2024-01-28 13:04:13 +0300

    Initial commit

---
+++ src/Circle.java
@@ -0,0 +1,16 @@
+public class Circle {
+    private static final double PI = 3.14;
+
+    public static void area() {
+     double    radius = PI/2;
+    double PI2 =  PI*radius*radius;
+        System.out.println(PI2);
+
+    }
+
+    public static void circumference(double radius) {
+        double d = PI*2*radius;
+        System.out.println(d);
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Circle circle = new Circle();
+        Circle.area();
+        Circle.circumference(1.57);
+
+
+    }
+}
